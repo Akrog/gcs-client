afebfd8f7857f6d43e7916a01a19b1132b4d8d6d
Gorka Eguileor <gorka@eguileor.com>
2015-10-12 11:46:38 +0200

    Add __init__ file
    Add __init__.py to gcs_client to facilitate the access to Objects from
    bucket, project, gcs_object, etc.

--- gcs_client/__init__.py
+++ gcs_client/__init__.py
@@ -3,7 +3,10 @@
 """Client Library for Google Cloud Storage."""
 from __future__ import absolute_import
 
-from gcs_client.gcs_client import *  # noqa
+from gcs_client.bucket import Bucket  # noqa
+from gcs_client.project import Project  # noqa
+from gcs_client.credentials import GCSCredential  # noqa
+from gcs_client.gcs_object import Object  # noqa
 
 
 __author__ = 'Gorka Eguileor'
