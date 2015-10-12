4013ec16ae7f232cb9eb5ff08ced8b478b0973bb
Gorka Eguileor <gorka@eguileor.com>
2015-10-12 11:41:04 +0200

    Add constants
    Add ACL, Projection, Scope and Storage Class constants.

---
+++ gcs_client/constants/acl.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+AUTH_READ = 'authenticatedRead'
+OWNER_FULL = 'bucketOwnerFullControl'
+OWNER_READ = 'bucketOwnerRead'
+PRIVATE = 'private'
+PROJECT_PRIVATE = 'projectPrivate'
+PUBLIC_R = 'publicRead'
+PUBLIC_RW = 'publicRead'
---
+++ gcs_client/constants/scope.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+READER = 'READER'
+WRITER = 'WRITER'
+OWNER = 'OWNER'
+CLOUD = 'CLOUD'
