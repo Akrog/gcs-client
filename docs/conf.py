9eae099659ba64503cacff4592ee336a55d39f22
Gorka Eguileor <geguileo@redhat.com>
2015-11-26 23:06:30 +0100

    Change copyright in the documentation
    Modify the copyright for the auto generated documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'gcs-client'
-copyright = u'2015, Gorka Eguileor'
+copyright = u'2015, Red Hat, Inc.'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
